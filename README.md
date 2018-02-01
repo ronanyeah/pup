cb4b1fcdf6977975de848a64fb837e36ca25032c
ronan <ronan_mccabe@protonmail.com>
2018-02-01 02:59:54 +0000

    done

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pup
-deploy purescript using up
+
+deploy [PureScript](http://www.purescript.org/) using [Up](https://up.docs.apex.sh/)
+
+- Requirements: `npm install -g purescript pulp bower up`
+- Dependencies: `bower install`
+- Build: `pulp build --optimise`
+- Run: `pulp run`/`node app.js`
+- Watch: `pulp --watch run`
+- Deploy: `up`
--- src/Main.purs
+++ src/Main.purs
@@ -2,30 +2,34 @@ module Main where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Node.Encoding (Encoding(..))
-import Node.HTTP (HTTP, ListenOptions, Request, Response, createServer, listen, responseAsStream, setStatusCode)
+import Node.Process (PROCESS, exit, lookupEnv)
+import Node.HTTP (HTTP, Request, Response, setHeader, createServer, listen, responseAsStream, setStatusCode)
 import Node.Stream (end, writeString)
-import Prelude (Unit, bind, discard, pure, show, unit, (<>))
-
-respond :: forall eff. Response -> Int -> String -> Eff (http :: HTTP | eff) Unit
-respond res status str =
-  do
-    let outputStream = responseAsStream res
-    setStatusCode res status
-    _ <- writeString outputStream UTF8 str (pure unit)
-    end outputStream (pure unit)
+import Prelude (Unit, bind, discard, pure, unit, (<>), (>>=), ($))
 
 app :: forall eff. Request -> Response -> Eff (http :: HTTP, console :: CONSOLE | eff) Unit
 app req res = do
-  respond res 200 "hi there"
-  log "responded"
+  let outputStream = responseAsStream res
+  setStatusCode res 200
+  setHeader res "Content-Type" "application/json"
+  _ <- writeString outputStream UTF8 "{\"ronan\":\"yeah\"}" (pure unit)
+  end outputStream (pure unit)
+  log "server responded!"
 
-listenOptions :: ListenOptions
-listenOptions = { hostname: "127.0.0.1", port: 8000, backlog: Nothing }
-
-main :: forall e. Eff ( http :: HTTP , console :: CONSOLE | e ) Unit
-main = do
-  let onReady = log ("server ready at http://" <> listenOptions.hostname <> ":" <> (show listenOptions.port))
-  server <- createServer app
-  listen server listenOptions onReady
+main :: forall e. Eff ( process :: PROCESS, http :: HTTP , console :: CONSOLE | e ) Unit
+main = lookupEnv "PORT" >>= case _ of
+  Nothing -> do
+    log $ "Unable to find PORT"
+    exit 1
+  Just portStr -> do
+    case fromString portStr of
+      Nothing -> do
+        log $ "Bad PORT"
+        exit 1
+      Just port -> do
+        let onReady = log ("server listening on port " <> portStr)
+        server <- createServer app
+        listen server { hostname: "", port: port, backlog: Nothing } onReady
