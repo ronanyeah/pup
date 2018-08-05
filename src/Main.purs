b406903676743d032c292b466d0aaaaa1dd81861
ronan <ronan_mccabe@protonmail.com>
2018-08-05 01:00:52 +0100

    update to ps 0.12

--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,17 @@
 module Main where
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Either (note, either)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Node.Encoding (Encoding(..))
-import Node.Process (PROCESS, exit, lookupEnv)
-import Node.HTTP (HTTP, Request, Response, setHeader, createServer, listen, responseAsStream, setStatusCode)
+import Node.Process (exit, lookupEnv)
+import Node.HTTP (Request, Response, setHeader, createServer, listen, responseAsStream, setStatusCode)
 import Node.Stream (end, writeString)
 import Prelude (Unit, bind, discard, pure, unit, (<>), (>>=), show, (>>>), (>=>))
+import Effect (Effect)
+import Effect.Console (log)
 
-main :: Eff ( process :: PROCESS, http :: HTTP, console :: CONSOLE ) Unit
+main :: Effect Unit
 main =
   lookupEnv "PORT"
   >>=
@@ -20,19 +20,19 @@ main =
   )
   >>> either logError start
 
-logError :: forall eff. String -> Eff ( process :: PROCESS, console :: CONSOLE | eff ) Unit
+logError :: String -> Effect Unit
 logError str = do
   log str
   exit 1
 
-start :: forall eff. Int -> Eff ( http :: HTTP, console :: CONSOLE | eff ) Unit
+start :: Int -> Effect Unit
 start port = do
   server <- createServer app
   listen server
     { hostname: "", port: port, backlog: Nothing }
     (log ("server listening on port " <> (show port)))
 
-app :: forall eff. Request -> Response -> Eff ( http :: HTTP | eff ) Unit
+app :: Request -> Response -> Effect Unit
 app req res = do
   let outputStream = responseAsStream res
   setStatusCode res 200
